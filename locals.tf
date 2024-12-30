57b576d42e896ee118551ad0af26f907b9fa8a94
Kvaara <karnavaara.jesse@gmail.com>
2024-12-30 18:56:17 +0200

    Utilizing `nonsensitive()` function as a way to circumvent Terraform's conservative behavior
    The `random_password` resource marks its generated
    password as sensitive, which is
    why it can't be outputted without
    the use of `nonsensitive()`

--- locals.tf
+++ locals.tf
@@ -1,7 +1,8 @@
 locals {
   db_config = {
-    user     = "admin"
-    password = random_password.password.result
+    user = "admin"
+    // nonsensitive() function removes the sensitive marking metadata, overriding Terraform's conservative behavior and allowing outputting.
+    password = nonsensitive(random_password.password.result)
     database = oci_mysql_mysql_db_system.this.display_name
     hostname = oci_mysql_mysql_db_system.this.endpoints[0].hostname
     port     = oci_mysql_mysql_db_system.this.endpoints[0].port
