fed9eb22a11a83f30fb78dd1f2a351dfb586f1eb
Kvaara <karnavaara.jesse@gmail.com>
2024-12-30 18:53:48 +0200

    Removed Unnecessary Comments, Implemented Health Check For LB

--- database.tf
+++ database.tf
@@ -14,15 +14,6 @@ resource "oci_mysql_mysql_db_system" "this" {
   admin_username      = "admin"
   description         = "MySQL database for multi-tiered application"
   display_name        = "${var.namespace}-mysql-db"
-  #   ip_address     = var.mysql_db_system_ip_address
-  #   port           = var.mysql_db_system_port
-  #   port_x         = var.mysql_db_system_port_x
-  #   source {
-  #     #Required
-  #     source_type = var.mysql_db_system_source_source_type
-
-  #     #Optional
-  #     # source_url = var.mysql_db_system_source_source_url
-  #     backup_id = oci_mysql_mysql_backup.test_backup.id
-  #   }
+  hostname_label      = "mysql-db"
+  port                = 3306
 }
--- lb.tf
+++ lb.tf
@@ -19,11 +19,6 @@ resource "oci_load_balancer_listener" "this" {
   name                     = "${var.namespace}-lb-listener"
   port                     = 80
   protocol                 = "HTTP"
-
-  #   hostname_names      = [oci_load_balancer_hostname.test_hostname.name]
-  #   path_route_set_name = oci_load_balancer_path_route_set.test_path_route_set.name
-  #   routing_policy_name = oci_load_balancer_load_balancer_routing_policy.test_load_balancer_routing_policy.name
-  #   rule_set_names      = [oci_load_balancer_rule_set.test_rule_set.name]
 }
 
 resource "oci_load_balancer_backend_set" "this" {
@@ -33,8 +28,8 @@ resource "oci_load_balancer_backend_set" "this" {
 
     port = 8080
     # response_body_regex = var.backend_set_health_checker_response_body_regex
-    # return_code         = var.backend_set_health_checker_return_code
-    # url_path            = var.backend_set_health_checker_url_path
+    return_code = 200
+    url_path    = "/"
   }
   load_balancer_id = oci_load_balancer_load_balancer.this.id
   name             = "${var.namespace}-lb-backend-set"
@@ -46,11 +41,4 @@ resource "oci_load_balancer_backend" "this" {
   ip_address       = oci_core_instance.this.private_ip
   load_balancer_id = oci_load_balancer_load_balancer.this.id
   port             = 8080
-
-  #Optional
-  #   backup          = var.backend_backup
-  #   drain           = var.backend_drain
-  #   max_connections = var.backend_max_connections
-  #   offline         = var.backend_offline
-  #   weight          = var.backend_weight
 }
